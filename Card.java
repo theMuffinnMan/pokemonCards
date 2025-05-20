76a15ea58211b442373e9ea22754c48f0f7cdecd
theMuffinnMan <164093037+theMuffinnMan@users.noreply.github.com>
2025-05-20 12:28:43 +1200

    Cards managment class and Card id attributes

--- Card.java
+++ Card.java
@@ -11,22 +11,26 @@ public class Card
     private String name;
     private int value;
     private String image;
+    private int id;
     static final String DEFUALT_IMAGE = "image name here"; // a defualt image value
     
 
     /**
      * Constructor for objects of class Card
      */
-    public Card(String nm, int vl, String img)
+    public Card(int key, String nm, int vl, String img)
     {
         // initialise instance variables
+        this.id = key;
         this.name = nm;
         this.value = vl;
-        // set image to defualt if no image is entered
+        // set image
         if(img == null){
+            // set image to defualt if no image is entered
             this.image = DEFUALT_IMAGE;
         }
         else{
+            // sets image to custom image
             this.image = img;
         }
     }
---
+++ Cards.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import ecs100.*;
+/**
+ * Managment class for hashmap of card collection
+ * 
+ * allows user to add cards to hashmap
+ * allows user to find cards in hashmap
+ * @Fleur
+ * @20/5
+ */
+public class Cards
+{
+    // fields
+    private HashMap<Integer, Card> collection; // declaring the collection hashmap
+    private int currCardId;
+
+    /**
+     * Constructor for objects of class Cards
+     */
+    public Cards()
+    {
+        collection = new HashMap<Integer, Card>();
+        
+        // initail cards in Collection
+        Card p1 = new Card(1, "Garchomp", 850, null);
+        Card p2 = new Card(2, "Umberon", 11000, null);
+        Card p3 = new Card(3, "Typhlosion", 300, null);
+        
+        this.currCardId = 3; // stores the current book id
+    }
+    
+    /**
+     * sets an id for the current card
+     */
+    public void setCardId(){
+        this.currCardId += 1;
+    }
+    
+    /**
+     * allows the user to add a card to the collection
+     * @card name
+     * @card value
+     * @card image
+     */
+    public void addCard(String name, int value, String image){
+        //sets card id
+        this.setCardId();
+        collection.put(this.currCardId, new Card(this.currCardId, name, value, image));
+    }
+}
